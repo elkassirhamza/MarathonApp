9e7e0c0580fd5a905d54d970671d13868f76d412
elkassir hamza <elkassir.hamza05@gmail.com>
2021-05-29 20:35:13 +0100

    V1.0.1

---
+++ app/src/main/java/com/example/marathonappv2/Slider.java
@@ -0,0 +1,34 @@
+package com.example.marathonappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Slider extends AppCompatActivity {
+
+    private Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+
+        b= findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openForm();
+            }
+        });
+    }
+
+    public void openForm(){
+        Intent intent = new Intent(this,Second.class);
+        startActivity(intent);
+    }
+
+
+}
